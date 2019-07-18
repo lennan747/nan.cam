7886d6c7afb7f62af9843db627b21735243e7b3f
lennan747 <lennan747@gmail.com>
2019-07-18 16:58:32 +0800

    Finish basic page layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLoacl() }}">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'nan')</title>
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
 <div id="app" class="{{ route_class() }}-page">
 
@@ -25,6 +28,7 @@
     @include('layouts._footer')
 </div>
 
+<!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
