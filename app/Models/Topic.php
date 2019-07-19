91af5612b62efb49e70e3f0b476fd114f28f0862
lennan747 <lennan747@gmail.com>
2019-07-19 17:17:29 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -8,7 +8,11 @@ class Topic extends Model
 
     public function category()
     {
-
+        return $this->belongsTo(Category::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
